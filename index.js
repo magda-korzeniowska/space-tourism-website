f5f9f66a7f0a5e2f3f56cacd724ee8f65f4ad558
magda-korzeniowska <m.korzeniowska@tlen.pl>
2022-08-10 14:35:45 +0200

    Add open/close button to mobile navigation

---
+++ index.js
@@ -0,0 +1,13 @@
+const navToggle = document.querySelector('.mobile-nav-toggle');
+const nav = document.querySelector('.primary-navigation');
+
+navToggle.addEventListener('click', () => {
+  const visibility = nav.getAttribute('data-visible');
+  if (visibility === 'false') {
+    nav.setAttribute('data-visible', true);
+    navToggle.setAttribute('aria-expanded', true);
+  } else {
+    nav.setAttribute('data-visible', false);
+    navToggle.setAttribute('aria-expanded', false);
+  }
+})
