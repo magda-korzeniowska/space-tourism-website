55ba85b6b1b76c5353c353960b4f9d9c93f6654a
magda-korzeniowska <m.korzeniowska@tlen.pl>
2022-08-23 16:40:35 +0200

    Add content change depending on active destination tab

--- tabs.js
+++ tabs.js
@@ -1,7 +1,11 @@
 const tabList = document.querySelector('[role="tablist"]');
 const tabs = tabList.querySelectorAll('[role="tab"]');
 
-tabList.addEventListener('keydown', changeTabFocus);
+tabList.addEventListener("keydown", changeTabFocus);
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", changeTabPanel);
+});
 
 let tabFocus = 0;
 
@@ -10,23 +14,51 @@ function changeTabFocus(e) {
   const keydownRight = 39;
 
   if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
-    tabs[tabFocus].setAttribute('tabindex', -1);
-  }
+    tabs[tabFocus].setAttribute("tabindex", -1);
 
-  if (e.keyCode === keydownRight) {
-    tabFocus++;
-    if (tabFocus >= tabs.length) {
-      tabFocus = 0;
+    if (e.keyCode === keydownRight) {
+      tabFocus++;
+      if (tabFocus >= tabs.length) {
+        tabFocus = 0;
+      }
+    } else if (e.keyCode === keydownLeft) {
+      tabFocus--;
+      if (tabFocus < 0) {
+        tabFocus = tabs.length - 1;
+      }
     }
+    tabs[tabFocus].setAttribute("tabindex", 0);
+    tabs[tabFocus].focus();
   }
+}
 
-  if (e.keyCode === keydownLeft) {
-    tabFocus--;
-    if (tabFocus < 0) {
-      tabFocus = tabs.length - 1;
-    }
-  }
+function changeTabPanel(e) {
+  const targetTab = e.target;
+  const targetPanel = targetTab.getAttribute("aria-controls");
+  const targetImage = targetTab.getAttribute("data-image");
 
-  tabs[tabFocus].setAttribute('tabindex', 0);
-  tabs[tabFocus].focus();
+  const tabContainer = targetTab.parentNode;
+  const mainContainer = tabContainer.parentNode;
+
+  tabContainer
+    .querySelector('[aria-selected="true"]')
+    .setAttribute("aria-selected", false);
+
+  targetTab.setAttribute("aria-selected", true);
+
+  hideContent(mainContainer, '[role="tabpanel"]');
+  showContent(mainContainer, [`#${targetPanel}`]);
+
+  hideContent(mainContainer, "picture");
+  showContent(mainContainer, [`#${targetImage}`]);
+}
+
+function hideContent(parent, content) {
+  parent
+    .querySelectorAll(content)
+    .forEach((item) => item.setAttribute("hidden", true));
+}
+
+function showContent(parent, content) {
+  parent.querySelector(content).removeAttribute("hidden");
 }
